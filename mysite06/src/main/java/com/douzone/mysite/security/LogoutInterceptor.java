185fa79560b7bea0b3e5dba3a3e2fe70660b9a3b
wpdnjsgh01 <wpdnjsgh02@naver.com>
2021-11-01 19:31:48 +0900

    mysite06 compile error occured

---
+++ mysite06/src/main/java/com/douzone/mysite/security/LogoutInterceptor.java
@@ -0,0 +1,26 @@
+package com.douzone.mysite.security;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LogoutInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		if(session == null) {
+			return false;
+		}
+		
+		session.removeAttribute("authUser");
+		session.invalidate();
+		
+		response.sendRedirect(request.getContextPath());
+		return false;
+	}
+
+}
